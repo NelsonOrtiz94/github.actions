80d807fc191afaa4dc345fc770e9b3c556a038f8
NelsonOrtiz94 <ortnn990@gmail.com>
2025-11-19 11:47:28 -0500

    Estructuración de Dockerfile

---
+++ src/test/java/com/github/actions/github/actions/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.actions.github.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
